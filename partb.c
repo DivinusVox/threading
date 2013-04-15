b108b74f995c3e193ca27906f9a1d3ee63c586a1
mapugina@gmail.com <mapugina@gmail.com>
2013-04-14 20:25:49 -0600

    finished and compiled partb

--- partb.c
+++ partb.c
@@ -1,3 +1,9 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void* incrementCounter( void* m );
+
 // Part B:
 // Make 4 threads, after running all, print value of count and exit.
 //      Wait for the threads to finish.
@@ -7,14 +13,36 @@ int count;
 void main()
 {
         count = 0;
-
-        //start thread 1
-        //start thread 2
-        //start thread 3
-        //start thread 4
+		
+		pthread_t t1,t2,t3,t4;
+		
+        pthread_create(&t1, NULL, incrementCounter, NULL); //start thread 1
+        pthread_create(&t2, NULL, incrementCounter, NULL); //start thread 2
+        pthread_create(&t3, NULL, incrementCounter, NULL); //start thread 3
+        pthread_create(&t4, NULL, incrementCounter, NULL); //start thread 4
 
 	//wait for threads to come back
-
+		pthread_join(t1, NULL);
+		pthread_join(t2, NULL);
+		pthread_join(t3, NULL);
+		pthread_join(t4, NULL);
         //print count
-        //exit
+		
+		printf("partb: count is %d\n", count);
+        return;//exit
+		
+}
+
+void* incrementCounter( void* m )
+{
+	int i;
+	for (i=0; i<10; i++)
+	{
+		int tempValue = count;
+		sleep(1);
+		tempValue = tempValue + 1;
+		count = tempValue;
+	}
+
+	return NULL;
 }
--- partd.c
+++ partd.c
@@ -2,19 +2,51 @@
 // Make 4 threads, after running all, print value of count and exit.
 //      Wait for the threads to finish. Use mutex.
 
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void* incrementCounter( void* m );
+
 int count;
+pthread_mutex_t mutexLock;
 
 void main()
 {
-        count = 0;
+	count = 0;
+	
+	pthread_t t1,t2,t3,t4;
+	
+	pthread_create(&t1, NULL, incrementCounter, NULL); //start thread 1
+	pthread_create(&t2, NULL, incrementCounter, NULL); //start thread 2
+	pthread_create(&t3, NULL, incrementCounter, NULL); //start thread 3
+	pthread_create(&t4, NULL, incrementCounter, NULL); //start thread 4
 
-        //start thread 1 w/ lock
-        //start thread 2 w/ lock
-        //start thread 3 w/ lock
-        //start thread 4 w/ lock
+	//wait for threads to come back
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+	pthread_join(t3, NULL);
+	pthread_join(t4, NULL);
+	//print count
+	
+	printf("partd: count is %d\n", count);
+	return;//exit
+}
 
-	//wait for thread to finish
 
-        //print count
-        //exit
+void* incrementCounter( void* m )
+{
+	int i;
+	pthread_mutex_init(&mutexLock); //lock here
+	
+	for (i=0; i<10; i++)
+	{
+		int tempValue = count;
+		sleep(1);
+		tempValue = tempValue + 1;
+		count = tempValue;
+	}
+	pthread_mutex_unlock(&mutexLock); //unlock here
+
+	return NULL;
 }
